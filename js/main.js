ab8a1137cde8fcb15abb1cd868e50a9ee8e6fbea
Vicki Grunewald <vicki.grunewald@gmail.com>
2015-09-16 18:23:39 -0700

    Add numba function expression

--- js/main.js
+++ js/main.js
@@ -15,3 +15,24 @@ function howdy(firstName, lastName, message) {
 
 var greeting = howdy( "Vicki", "Grunewald", "hi");
 console.log( greeting );
+
+var numba = function( string ) {
+	if (string.length < 7) {
+		console.log( "What a short little word!");
+	}	else if (string.length > 7) {
+		console.log( "I'm sorry, but that's too many to count.");
+	}	else if (string.length = 7) {
+		console.log( "7, what a perfect choice!");
+	}
+};
+
+numba("7");
+
+numba("seventy");
+
+numba("university");
+
+
+
+
+
